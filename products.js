5a04a2b58239a988b07cb36fadbf8da800723414
LongDoan1205 <longdoan20041205@gmail.com>
2025-03-20 16:20:00 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,5 @@
+const products = [
+    { id: 1, name: "Áo thun nam", price: 150000, image: "images/product1.jpg" },
+    { id: 2, name: "Quần jean nữ", price: 250000, image: "images/product2.jpg" },
+    { id: 3, name: "Giày sneaker", price: 500000, image: "images/product3.jpg" }
+];
